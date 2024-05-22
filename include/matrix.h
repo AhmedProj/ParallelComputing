57043e6e4d9cc984b39613c5b6ce0be219b111cb
Ahmed <117286352+AhmedProj@users.noreply.github.com>
2024-05-22 22:25:40 +0200

    matrix class definition

---
+++ include/matrix.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class Matrix {
+private:
+    float* data;
+    int rows;
+    int columns;
+
+public:
+    Matrix(int n, int m);
+    ~Matrix();
+    Matrix(const Matrix& matrix);
+    void set_value(int i, int j, float value);
+    float get_value(int i, int j);
+    void set_all();
+    void print_matrix();        
+    static Matrix multiplication(const Matrix& x, const Matrix& y);
+    static Matrix parallel_multiplication(const Matrix& x, const Matrix& y);
+};
